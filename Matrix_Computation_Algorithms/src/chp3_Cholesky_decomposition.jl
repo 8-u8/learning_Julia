371ddb5fc614811701c7d0d35d06e1f4f3ad3b07
8-u8 <24557402+8-u8@users.noreply.github.com>
2026-01-15 09:44:26 +0900

    微修正

--- Matrix_Computation_Algorithms/src/chp3_Cholesky_decomposition.jl
+++ Matrix_Computation_Algorithms/src/chp3_Cholesky_decomposition.jl
@@ -3,43 +3,47 @@ using LinearAlgebra
 
 # %% cholesky decomposition
 function cholesky_decomposition(A::Matrix{Float64})
-    n = size(A, 1)
-    L = copy(LowerTriangular(float(A)))
-    for j = 1:n
-      L[j, j] = sqrt(L[j, j])
-      L[j+1:n, j] = L[j+1:n, j] / L[j, j]
-      for i = j+1:n
-        L[i, j+1:i] -= L[i, j] * L[j+1:i, j]
-      end
+  n = size(A, 1)
+  L = copy(LowerTriangular(float(A)))
+  for j = 1:n
+    L[j, j] = sqrt(L[j, j])
+    L[j+1:n, j] = L[j+1:n, j] / L[j, j]
+    for i = j+1:n
+      L[i, j+1:i] -= L[i, j] * L[j+1:i, j]
     end
-    return L
+  end
+  return L
 end
 
 # %% LDLt decomposition
 function LDLt_decomposition(A::Matrix{Float64})
-    n = size(A, 1)
-    L = copy(LowerTriangular(float(A)))
-    D = zeros(Float64, n)
-    for j = 1:n
-      D[j] = L[j, j]
-      L[j, j] = 1.0
-      L[j+1:n, j] = L[j+1:n, j] / D[j]
-      for i = j+1:n
-        L[i, j+1:i] -= D[j] * L[i, j] * L[j+1:i, j]
-      end
+  n = size(A, 1)
+  L = copy(LowerTriangular(float(A)))
+  D = zeros(Float64, n)
+  for j = 1:n
+    D[j] = L[j, j]
+    L[j, j] = 1.0
+    L[j+1:n, j] = L[j+1:n, j] / D[j]
+    for i = j+1:n
+      L[i, j+1:i] -= D[j] * L[i, j] * L[j+1:i, j]
     end
-    return (L=L, D=D)
+  end
+  return (L=L, D=D)
+end
 
 # %% example
 Aspd = [4 2 3; 2 5 5.5; 3 5.5 8.5]
 t = @elapsed L = cholesky_decomposition(Aspd)
-println("Cholesky Decomposition L:"); display(L)
+println("Cholesky Decomposition L:")
+display(L)
 println("residual norm: ", norm(Aspd - L * L'))
 println("Time elapsed for Cholesky Decomposition: ", t, " seconds")
 
 F = LDLt_decomposition(Aspd)
-println("LDLt Decomposition L:"); display(F.L)
-println("LDLt Decomposition D:"); display(F.D)
-println("residual norm: ", norm(Aspd - F.L * diagm(F.D) * F.L')) 
+println("LDLt Decomposition L:")
+display(F.L)
+println("LDLt Decomposition D:")
+display(F.D)
+println("residual norm: ", norm(Aspd - F.L * diagm(F.D) * F.L'))
 
 end # module Matrix_Computation_Algorithms
